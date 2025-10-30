730673dcea1ccd5db54d1255121e48ff0b0e8c4a
Lucas Leprevost <lucasleprevost9@gmail.com>
2025-10-30 17:56:04 +0100

    Creation du docker-compose

--- src/main/java/com/lucas/gestionnaire_taches/GestionnaireTachesApplication.java
+++ src/main/java/com/lucas/gestionnaire_taches/GestionnaireTachesApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GestionnaireTachesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GestionnaireTachesApplication.class, args);
-	}
+		SpringApplication.run(GestionnaireTachesApplication.class, args);}
 
 }
