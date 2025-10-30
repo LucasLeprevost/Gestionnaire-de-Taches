0cb7d97a494d9304e68b2ed0836162064268324b
Lucas Leprevost <lucasleprevost9@gmail.com>
2025-10-30 15:51:50 +0100

    Initailisation du gestionnaire de taches

---
+++ src/main/java/com/lucas/gestionnaire_taches/GestionnaireTachesApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.gestionnaire_taches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnaireTachesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionnaireTachesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/gestionnaire_taches/GestionnaireTachesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.gestionnaire_taches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionnaireTachesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
