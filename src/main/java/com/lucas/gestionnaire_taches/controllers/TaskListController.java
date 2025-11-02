89dec125c65d9278d351afe0144b7b292a098664
Lucas Leprevost <lucasleprevost9@gmail.com>
2025-11-02 15:38:36 +0100

    Ajout de commentaires dans controller

--- src/main/java/com/lucas/gestionnaire_taches/controllers/TaskListController.java
+++ src/main/java/com/lucas/gestionnaire_taches/controllers/TaskListController.java
@@ -33,13 +33,22 @@ public class TaskListController
     }
 
 
+    /**
+     * Gère la requête HTTP POST sur /api/task-lists pour créer une nouvelle liste de tâches.
+     *
+     * Reçoit un DTO du client, appelle la logique métier (Service) pour sauvegarder la liste,
+     * puis retourne le DTO mis à jour, incluant l'ID généré par la base de données.
+     *
+     * @param taskListDto Le DTO des données envoyées par le client.
+     * @return Le DTO de liste de tâches complété (avec l'ID et les dates générées).
+     */
     @PostMapping
     public TaskListDto createTaskList(@RequestBody TaskListDto taskListDto)
     {
-        TaskList createTaskList = this.taskListService.createTaskList(
+        TaskList createdTaskList = this.taskListService.createTaskList(
                 this.taskListMapper.fromDto(taskListDto)
         );
 
-        return this.taskListMapper.toDto(createTaskList);
+        return this.taskListMapper.toDto(createdTaskList);
     }
 }
